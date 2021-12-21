39cb80ebd2d06856a76aee992711f176725d5095
karlzx <karlsomoray@gmail.com>
2021-12-21 22:51:20 +1000

    Initial Code!

---
+++ frontend.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.header("Date Night (tm) 👀")
+
+
+activity_df = pd.read_excel('NewActivity.xlsx')
+
+
+price = activity_df['Price'].unique()
+
+options = st.multiselect('What Price Range?',price)
+activity_df = activity_df[activity_df["Price"].isin(options)]
+
+print(activity_df)
+col1, col2 = st.columns(2)
+
+
+
+with col1:
+     st.map(activity_df) 
+with col2:
+     st.table(activity_df[["Short Activity","Price"]])
+print(price)
---
+++ main.py
@@ -0,0 +1,30 @@
+# importing geopy library
+from geopy.geocoders import Nominatim
+import pandas as pd
+
+
+# # Activity to Locations
+# df = pd.read_excel('Activity.xlsx')
+# df =  df.truncate(after = 56)
+
+# geolocator = Nominatim(user_agent="locs_py")
+
+# for i in range(len(df['Location/Place (Beta)'])):
+#     print(df.loc[i,'Location/Place (Beta)'])
+
+#     location = geolocator.geocode(df.loc[i,'Location/Place (Beta)'] + " Brisbane Queensland")
+#     if location is None:
+#         location = geolocator.geocode(df.loc[i,'Location/Place (Beta)'] +  " Queensland")
+#     df.loc[i,'latitude'] = location.latitude
+#     df.loc[i,'longitude'] = location.longitude
+#     print(location)
+# df.to_excel('NewActivity.xlsx')
+df = pd.read_excel('NewActivity.xlsx')
+
+price = df['Price'].unique()
+
+print(price)
+# New to Analysis
+# geolocator = Nominatim(user_agent="locs_py")
+# location = geolocator.geocode("Mount Coot-tha Brisbane QLD")
+# print(location.address)
